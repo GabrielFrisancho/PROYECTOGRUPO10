9b1480ed708183366b758ea7cec42c24439d1f5f
Adrian <166168649+ADRIAN0819@users.noreply.github.com>
2024-07-15 14:49:46 -0500

    VERSION 8 UserPreferences.h

--- UserPreferences.h
+++ UserPreferences.h
@@ -3,21 +3,37 @@
 
 #include <vector>
 #include <QString>
+#include <QDate>
 #include <mutex>
+
+struct WatchLaterMovie {
+    QString title;
+    int priority;
+    QString notes;
+    QDate plannedDate;
+    bool viewed;
+
+    WatchLaterMovie(const QString& t) : title(t), priority(0), notes(""), plannedDate(QDate::currentDate()), viewed(false) {}
+};
 
 class UserPreferences {
 public:
     static UserPreferences* getInstance();
     void addLike(const QString &movieTitle);
     void addWatchLater(const QString &movieTitle);
+    void setPriority(const QString &movieTitle, int priority);
+    void addNote(const QString &movieTitle, const QString &note);
+    void setPlannedDate(const QString &movieTitle, const QDate &date);
+    void markAsViewed(const QString &movieTitle);
     std::vector<QString> getLikes() const;
-    std::vector<QString> getWatchLater() const;
+    std::vector<WatchLaterMovie> getWatchLater() const;
+    void sortWatchLater(const QString &criterion);
 
 private:
     UserPreferences() = default;
     static UserPreferences* instance;
     std::vector<QString> likes;
-    std::vector<QString> watchLater;
+    std::vector<WatchLaterMovie> watchLater;
     mutable std::mutex mutex;
 };
 
