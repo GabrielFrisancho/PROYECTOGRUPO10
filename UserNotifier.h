b206ebdaa471f6a6fa2bda2ca1ed3847e8e06bde
Adrian <166168649+ADRIAN0819@users.noreply.github.com>
2024-07-15 14:58:58 -0500

    VERSION 9 UserNotifier.h

--- UserNotifier.h
+++ UserNotifier.h
@@ -3,6 +3,7 @@
 
 #include "Observer.h"
 #include <iostream>
+#include <QString>
 
 class UserNotifier : public Observer {
 private:
@@ -14,6 +15,10 @@ public:
     void update(const QString& movieTitle) override {
         std::cout << "Hey " << username.toStdString() << "! A new movie matching your preferences has been added: "
                   << movieTitle.toStdString() << std::endl;
+    }
+
+    void setUsername(const QString& name) {
+        username = name;
     }
 };
 
